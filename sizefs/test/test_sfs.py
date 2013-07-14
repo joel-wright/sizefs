bc84ed8a5604967a108163f91f0e2386dee53ed5
Joel Wright <joel.wright@gmail.com>
2013-07-14 22:36:24 +0200

    Update SizeFS object test to use expected os usage pattern

--- sizefs/test/test_sfs.py
+++ sizefs/test/test_sfs.py
@@ -6,8 +6,10 @@ import re
 sfs = SizeFS()
 
 def test_regex_dir():
-    sfs.add_regex_dir("regex1","a(bcd)*e{4}")
-    regex_file = sfs.open('regex1/128KB')
-    regex_file_contents = regex_file.read()
-    match = re.match("a(bcd)*e{4}",regex_file_contents)
+    sfs.mkdir("/regex1")
+    sfs.setxattr("regex1", "filler", "a(bcd)*e{4}")
+    sfs.create("/regex1/128K")
+    regex_file = sfs.open("/regex1/128K")
+    regex_file_contents = regex_file.read("/regex1/128K", 128*1024, 0)
+    match = re.match("a(bcd)*e{4}", regex_file_contents)
     assert (len(regex_file_contents) == 131072 and not match is None)
