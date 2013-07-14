28a63d2d780d52983d7a92fcb5d6700d8e1374fc
Joel Wright <joel.wright@gmail.com>
2013-07-14 22:25:39 +0200

    Update tests for new generator

--- sizefs/test/test_contents.py
+++ sizefs/test/test_contents.py
@@ -1,45 +1,45 @@
 __author__ = 'jjw'
 
-from sizefs.contents import Filler
+from sizefs.contents import XegerGen
 import re
 
 def test_simple():
-    filler = Filler(regenerate=False,pattern="0",max_random=128)
-    contents = filler.fill(16)
+    generator = XegerGen(1024, filler="0", max_random=10)
+    contents = generator.read(0, 16)
     assert contents == "0000000000000000"
 
 def test_repeat():
-    filler = Filler(regenerate=False,pattern="ab",max_random=128)
-    contents = filler.fill(16)
+    generator = XegerGen(1024, filler="ab", max_random=10)
+    contents = generator.read(0, 16)
     assert contents == "abababababababab"
 
 def test_star():
-    filler = Filler(regenerate=True,pattern="a(bc)*d",max_random=128)
-    contents = filler.fill(256)
-    match = re.match("a(bc)*d",contents)
-    assert not match is None
+    generator = XegerGen(1024, filler="a(bc)*d", max_random=10)
+    contents = generator.read(0, 256)
+    match = re.match("a(bc)*d", contents)
+    assert match is not None
 
 def test_plus():
-    filler = Filler(regenerate=True,pattern="a(bc)+d",max_random=128)
-    contents = filler.fill(256)
-    match = re.match("a(bc)+d",contents)
-    assert not match is None
+    generator = XegerGen(1024, filler="a(bc)+d", max_random=10)
+    contents = generator.read(0, 256)
+    match = re.match("a(bc)+d", contents)
+    assert match is not None
 
 def test_numbered_repeat():
-    filler = Filler(regenerate=True,pattern="a(bc){5}d",max_random=128)
-    contents = filler.fill(16)
+    generator = XegerGen(1024, filler="a(bc){5}d", max_random=10)
+    contents = generator.read(0, 16)
     assert contents == "abcbcbcbcbcdabcb"
 
 def test_choice():
-    filler = Filler(regenerate=True,pattern="a[012345]{14}b",max_random=128)
-    contents = filler.fill(256)
-    match = re.match("a[012345]{14}b",contents)
-    assert not match is None
+    generator = XegerGen(1024, filler="a[012345]{14}b", max_random=10)
+    contents = generator.read(0, 256)
+    match = re.match("a[012345]{14}b", contents)
+    assert match is not None
 
 def test_range():
-    filler = Filler(regenerate=True,pattern="a[0-9,a-z,A-Z]{5}d",max_random=128)
-    contents = filler.fill(256)
-    match = re.match("a[0-9,a-z,A-Z]{5}d",contents)
-    assert not match is None
+    generator = XegerGen(1024, filler="a[0-9,a-z,A-Z]{5}d", max_random=10)
+    contents = generator.read(0, 256)
+    match = re.match("a[0-9,a-z,A-Z]{5}d", contents)
+    assert match is not None
 
 
