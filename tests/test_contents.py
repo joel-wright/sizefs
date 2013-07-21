cc157dd7e477f31950cbd5eb30a4e090d913e92a
Joel Wright <joel.wright@gmail.com>
2013-07-21 23:25:17 +0200

    Make unittest test cases

--- tests/test_contents.py
+++ tests/test_contents.py
@@ -9,35 +9,30 @@ class XegerGenTestCase(unittest.TestCase):
 
     def test_simple(self):
         generator = XegerGen(1024, filler="0", max_random=10)
-        contents = generator.read(0, 16)
+        contents = generator.read(0, 15)
         assert contents == "0000000000000000"
-
 
     def test_repeat(self):
         generator = XegerGen(1024, filler="ab", max_random=10)
-        contents = generator.read(0, 16)
+        contents = generator.read(0, 15)
         assert contents == "abababababababab"
-
 
     def test_star(self):
         generator = XegerGen(1024, filler="a(bc)*d", max_random=10)
-        contents = generator.read(0, 256)
+        contents = generator.read(0, 255)
         match = re.match("a(bc)*d", contents)
         assert match is not None
 
-
     def test_plus(self):
         generator = XegerGen(1024, filler="a(bc)+d", max_random=10)
-        contents = generator.read(0, 256)
+        contents = generator.read(0, 255)
         match = re.match("a(bc)+d", contents)
         assert match is not None
 
-
     def test_numbered_repeat(self):
         generator = XegerGen(1024, filler="a(bc){5}d", max_random=10)
-        contents = generator.read(0, 16)
+        contents = generator.read(0, 15)
         assert contents == "abcbcbcbcbcdabcb"
-
 
     def test_choice(self):
         generator = XegerGen(1024, filler="a[012345]{14}b", max_random=10)
@@ -52,4 +47,5 @@ class XegerGenTestCase(unittest.TestCase):
         match = re.match("a[0-9,a-z,A-Z]{5}d", contents)
         assert match is not None
 
-
+if __name__ == '__main__':
+    unittest.main()
